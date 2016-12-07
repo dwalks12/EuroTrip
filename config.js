b33faf598b662f252b02904aa292b4dc86fb291a
Dawson Walker <dawsonwalker@Dawsons-MacBook-Pro.local>
2016-12-07 10:50:47 +0100

    updated package

--- config.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.set('port', (process.env.PORT || 3000));
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('builds/test'));
-}
