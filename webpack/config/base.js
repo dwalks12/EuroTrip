78cd74749058476ec7b08929183444baf2f6a76f
Dawson Walker <dawsonwalker@Dawsons-MacBook-Pro.local>
2016-12-07 12:36:56 +0100

    Creating 'staging' build. [Wed  7 Dec 2016 12:36:56 CET]

--- webpack/config/base.js
+++ webpack/config/base.js
@@ -26,7 +26,7 @@ const config = {
 		],
 	},
 	output: {
-		path: path.join(ROOT_PATH, 'web'),
+		path: path.join(ROOT_PATH, './src/html/index.html'),
 		filename: '[name].bundle.js',
 	},
 	resolve: {
