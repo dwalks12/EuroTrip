960725a18b97db673e2f522af1f6a131d0036a32
Dawson Walker <dawsonwalker@Dawsons-MacBook-Pro.local>
2016-12-08 14:44:37 +0100

    updated server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var proxy = httpProxy.createProxyServer();
 var app = express();
 var router = express.Router();
 var mongodb = require('mongodb');
-var ObjectId mongodb.ObjectID;
+var ObjectId = mongodb.ObjectID;
 
 var IMAGES_COLLECTION = 'images;'
 var isProduction = process.env.NODE_ENV === 'production';
@@ -35,8 +35,10 @@ if(!isProduction) {
   });
 }
 var db;
+var DATABASE_URL = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://Dawson:Pokemon1@ds127938.mlab.com:27938/heroku_bj9c2mnh';
 
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+console.log(DATABASE_URL);
+mongodb.MongoClient.connect(DATABASE_URL, function (err, database) {
   if(err) {
     console.log(err);
     process.exit(1);
@@ -45,11 +47,62 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   db = database;
   console.log("Successfully connected to database");
   //important to catch any errors from proxy or server will crash
-  proxy.on('error', function(e) {
-    console.log('Could not connect to proxy, please try again...');
-  });
 
-  app.listen(port, function() {
-    console.log('Server running on port ' + port);
+});
+
+//routes
+proxy.on('error', function(e) {
+  console.log('Could not connect to proxy, please try again...');
+});
+
+app.listen(port, function() {
+  console.log('Server running on port ' + port);
+});
+//generic error handler
+function handleError(res, reason, message, code) {
+  console.log('error: ' + reason);
+  res.status(code || 500).json({'error': message});
+}
+
+app.get('/images', function(req, res) {
+
+});
+
+app.post('/images', function(req, res) {
+  var newImage = req.body;
+  newImage.createdAt = new Date();
+  if(!(req.body.createdBy)) {
+    handleError(res, 'Invalid user input', 'Must provide a createdBy name.', 400);
+  }
+  db.collection(IMAGES_COLLECTION).insertOne(newContact, function(err, doc) {
+    if(err) {
+      handleError(res, err.message, 'Failed to create image');
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
   });
 });
+
+// app.get('/images:id', function(req, res) {
+//
+// });
+//
+// app.delete('/images:id', functions(req, res) {
+//
+// });
+
+
+/*
+object schema:
+
+{
+  "id": <objid>,
+  "image": <string>
+  "imageid": <string>
+  "createdAt": <string>
+  "createdBy": <string>
+}
+
+
+
+*/
