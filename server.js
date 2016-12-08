6348125c2b006c664f42b1de4ed725533a590f68
Dawson Walker <dawsonwalker@Dawsons-MacBook-Pro.local>
2016-12-08 15:25:32 +0100

    test

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.post('/images', function(req, res) {
   var newImage = req.body;
   newImage.createdAt = new Date();
   if(!(req.body.createdBy)) {
-    handleError(res, 'Invalid user input', 'Must provide a createdBy name.'+req.body.createdBy, 400);
+    handleError(res, 'Invalid user input', 'Must provide a createdBy name.'+req.body[0] + req.body[0].imageUrl, 400);
   }
   db.collection(IMAGES_COLLECTION).insertOne(newContact, function(err, doc) {
     if(err) {
