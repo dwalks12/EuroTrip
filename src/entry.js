6c0a879efb299042dfe1ffdae25b3a79255e6aa0
Dawson Walker <dawsonwalker@Dawsons-MacBook-Pro.local>
2016-12-09 12:03:29 +0100

    updated hash history so it doesnt show query keys

--- src/entry.js
+++ src/entry.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createHistory } from 'history';
+import { createHistory, useBasename, createHashHistory } from 'history';
 import useScroll from 'scroll-behavior/lib/useStandardScroll';
 
 var BrowserHistory = require('react-router/lib/browserHistory').default;
+import createBrowserHistory from 'history/lib/createBrowserHistory';
 import shouldUpdateScroll from './utility/shouldUpdateScroll';
 import { useRouterHistory, browserHistory } from 'react-router';
 import App from './App';
@@ -13,17 +14,18 @@ import './styling/global.css';
 
 const basename = ``;
 
-const history = useScroll(useRouterHistory(createHistory))({
-	basename,
-	shouldUpdateScroll,
-});
-
+// const history = useScroll(useRouterHistory(createHistory))({
+// 	basename,
+// 	shouldUpdateScroll,
+// });
+// const history = useBasename(createHistory)({basename});
+const history = useRouterHistory(createHashHistory)({queryKey: false});
 const root = document.getElementById('root');
 
 const render = () => {
 	const WrappedApp = () =>
 		<div>
-			<App history={BrowserHistory} />
+			<App history={history} />
 		</div>
 	;
 
